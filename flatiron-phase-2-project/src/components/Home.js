1c9dbc9dfb6513c238e6eb65522c0c355dfd9b1d
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-06-28 01:07:19 -0400

    move the fetch on the 'Recipe' component and display the name and image from the server

--- flatiron-phase-2-project/src/components/Home.js
+++ flatiron-phase-2-project/src/components/Home.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  const [recipe, setRecipe] = useState([]);
-
-  useEffect(() => {
-    fetch("https://kmok-server.onrender.com/recipes")
-      .then((r) => r.json())
-      .then((items) => console.log(items));
-  }, []);
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+      <Link to={"/recipe"}>Recipe</Link>
+    </>
+  );
 }
 
 export default Home;
--- flatiron-phase-2-project/src/components/Recipe.js
+++ flatiron-phase-2-project/src/components/Recipe.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Recipe() {
-  return <div>Recipe</div>;
+  const [recipe, setRecipe] = useState([]);
+
+  useEffect(() => {
+    fetch("https://kmok-server.onrender.com/recipes")
+      .then((r) => r.json())
+      .then((items) => setRecipe(items));
+  }, []);
+  return (
+    <div>
+      {recipe.map((food) => {
+        return (
+          <>
+            <ul>{food.name}</ul>
+            <img src={food.image} alt="Food Image" />
+          </>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Recipe;
