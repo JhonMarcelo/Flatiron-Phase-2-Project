55f79986a6cc738062503a3472bea7aa16888253
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-06-28 09:55:50 -0400

    adds bootstrap card on recipe to be mapped from the fetched data

--- flatiron-phase-2-project/src/components/Recipe.js
+++ flatiron-phase-2-project/src/components/Recipe.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import Form from "./Form";
+import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
 
 function Recipe() {
   const [recipe, setRecipe] = useState([]);
@@ -10,11 +13,28 @@ function Recipe() {
   }, []);
   return (
     <div>
+      <Form />
       {recipe.map((food) => {
         return (
           <>
-            <ul>{food.name}</ul>
-            <img src={food.image} alt="Food Image" />
+            <Card>
+              <Card.Img variant="top" src={food.image} alt="food_image" />
+              <Card.Body>
+                <Card.Text>
+                  <h2>Ingredients</h2>
+                  {food.ingredients.map((ingredients) => {
+                    return <ul>{ingredients}</ul>;
+                  })}
+                </Card.Text>
+                <Card.Text>
+                  <h2>Directions</h2>
+                  {food.directions.map((directions) => {
+                    return <ul>{directions}</ul>;
+                  })}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <br />
           </>
         );
       })}
