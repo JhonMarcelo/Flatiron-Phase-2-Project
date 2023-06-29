b9fbdcafdd16d5b4c496bbe8d7a3309cc9c45707
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-06-29 02:42:38 -0400

    add button and card from bootstrap to Home component

--- flatiron-phase-2-project/src/components/Home.js
+++ flatiron-phase-2-project/src/components/Home.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import banner from "../images/KMOK.png";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function Home() {
   return (
     <>
       <img class="banner" src={banner} alt="banner" />
-      <div>Home</div>
-      <Link to={"/recipe"}>Recipe</Link>
+      Check out
+      <Link to={"/recipe"}> Recipes </Link>
+      to get started!
     </>
   );
 }
