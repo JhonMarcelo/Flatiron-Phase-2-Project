52a6709d735c3c7e6cfe4e8d86a0b0b79973a0cb
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-06-29 02:53:59 -0400

    adds a card from bootstrap and attached photo in to it.

--- flatiron-phase-2-project/src/components/Home.js
+++ flatiron-phase-2-project/src/components/Home.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import banner from "../images/KMOK.png";
+import creamy from "../images/creamy-garlic.jpg";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
@@ -10,6 +11,23 @@ function Home() {
       Check out
       <Link to={"/recipe"}> Recipes </Link>
       to get started!
+      <br></br>
+      <br></br>
+      <div>
+        <h1>
+          <strong>Easy Keto Recipes</strong>
+        </h1>
+      </div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={creamy} />
+        <Card.Body>
+          <Card.Title>Creamy Garlic Chicken</Card.Title>
+          <Card.Text>
+            You dont have to feel bad eating this delicious food as it will fit
+            your macros!
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </>
   );
 }
