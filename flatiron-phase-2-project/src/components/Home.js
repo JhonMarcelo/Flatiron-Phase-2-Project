ea36990668edba4e4b7872df0aa896f02a626a72
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-06-29 03:01:22 -0400

    adds another card with the pizza card

--- flatiron-phase-2-project/src/components/Home.js
+++ flatiron-phase-2-project/src/components/Home.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import banner from "../images/KMOK.png";
 import creamy from "../images/creamy-garlic.jpg";
+import pizza from "../images/pizza.jpg";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Placeholder from "react-bootstrap/Placeholder";
 
 function Home() {
   return (
@@ -18,16 +20,27 @@ function Home() {
           <strong>Easy Keto Recipes</strong>
         </h1>
       </div>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={creamy} />
-        <Card.Body>
-          <Card.Title>Creamy Garlic Chicken</Card.Title>
-          <Card.Text>
-            You dont have to feel bad eating this delicious food as it will fit
-            your macros!
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <div className="d-flex justify-content-around">
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={creamy} />
+          <Card.Body>
+            <Card.Title>Creamy Garlic Chicken</Card.Title>
+            <Card.Text>
+              You dont have to feel bad eating this delicious food as it will
+              fit your macros!
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={pizza} />
+          <Card.Body>
+            <Card.Title>Cauliflower Crust Pizza</Card.Title>
+            <Card.Text>
+              Craving for pizza? No problem! We got you covered!
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   );
 }
