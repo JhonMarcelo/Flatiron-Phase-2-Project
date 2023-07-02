7f176b9d628fb83cf92be0d6a7f1cf38d7f9c114
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-07-02 11:23:34 -0400

    adds delete button on each card

--- flatiron-phase-2-project/src/components/Recipe.js
+++ flatiron-phase-2-project/src/components/Recipe.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import ModalForm from "./ModalForm";
 import Card from "react-bootstrap/Card";
 
+import Button from "react-bootstrap/Button";
+
 function Recipe() {
   const [recipe, setRecipe] = useState([]);
 
@@ -15,35 +17,57 @@ function Recipe() {
     setRecipe([...recipe, newRecipe]);
   }
 
+  function handleDeleteRecipe(id) {
+    const newRecipeList = recipe.filter((recipe) => recipe.id !== id);
+    setRecipe(newRecipeList);
+
+    console.log(id);
+    // fetch(`http://localhost:3003/recipes/${id}`, {
+    //   method: "DELETE",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(newRecipe),
+    // })
+    //   .then((r) => r.json())
+    //   .then((newItem) => onSubmitForm(newItem));
+  }
+
   return (
     <div>
-      {recipe.map((food) => {
-        return (
-          <>
-            <Card key={food.id}>
-              <Card.Img variant="top" src={food.image} alt="food_image" />
-              <Card.Body>
-                <Card.Text>
-                  <h1>{food.name}</h1>
-                  <h2>Ingredients</h2>
-                  {food.ingredients.map((ingredients) => {
-                    return <ul>{ingredients}</ul>;
-                  })}
-                </Card.Text>
-                <Card.Text>
-                  <h2>Directions</h2>
-                  {food.directions.map((directions) => {
-                    return <ul>{directions}</ul>;
-                  })}
-                </Card.Text>
-                <ModalForm onSubmitForm={handleSubmitForm} />
-              </Card.Body>
-            </Card>
+      <ul>
+        {recipe.map((food) => {
+          return (
+            <>
+              <Card key={food.id}>
+                <Card.Img variant="top" src={food.image} alt="food_image" />
+                <Card.Body>
+                  <Card.Text>
+                    <h1>{food.name}</h1>
+                    <h2>Ingredients</h2>
+                    {food.ingredients.map((ingredients) => {
+                      return <ul key={crypto.randomUUID()}>{ingredients}</ul>;
+                    })}
+                  </Card.Text>
+                  <Card.Text>
+                    <h2>Directions</h2>
+                    {food.directions.map((directions) => {
+                      return <ul key={crypto.randomUUID()}>{directions}</ul>;
+                    })}
+                  </Card.Text>
+                  <ModalForm onSubmitForm={handleSubmitForm} />
+                  <span> </span>
+                  <Button onClick={() => handleDeleteRecipe(food.id)}>
+                    Delete
+                  </Button>
+                </Card.Body>
+              </Card>
 
-            <br />
-          </>
-        );
-      })}
+              <br />
+            </>
+          );
+        })}
+      </ul>
     </div>
   );
 }
